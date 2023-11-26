a6ce860c12c99e3ae1a5e86c52ac9d9c8d3f0399
Hector Garrido <hector.garrido@direcciondecompras.local>
2023-11-26 20:30:56 -0300

    El script ScheduleR es para automatizar el índice, tengo que editarlo aún, pero priorizaré la presentación

---
+++ scheduleR.R
@@ -0,0 +1,12 @@
+load_pkg <- function(pack){
+  create.pkg <- pack[!(pack %in% installed.packages()[, "Package"])]
+  if (length(create.pkg))
+    install.packages(create.pkg, dependencies = TRUE)
+  sapply(pack, require, character.only = TRUE)
+}
+
+packages = c("taskscheduleR")
+
+load_pkg(packages)
+
+
